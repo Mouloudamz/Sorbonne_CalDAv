90ed15a94a2083fcbc253c80a2ed9989ce91e2cf
Mouloud AMZALI <127723774+Mouloudamz@users.noreply.github.com>
2024-09-15 11:56:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -184,9 +184,9 @@ def batch_update_events(events, calendar_id):
             'end': end,
         }
         # Colorize events that contain 'MU4IN057' in the summary
-        if 'MU4IN057' in event['summary']:
-            event_body['colorId'] = '10'  # 11 is a color code for red, you can choose any
-
+        if 'MU5IN057' or 'MU5IN067' in event['summary']:
+            event_body['colorId'] = '8'  
+            
         # Add each event insert request to the batch
         batch.add(service.events().insert(calendarId=calendar_id, body=event_body))
     
